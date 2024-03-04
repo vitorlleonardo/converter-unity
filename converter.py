cf64c3cae8f5fc4f0a0915e47192d0b5190d67f7
vitorlleonardo <joaovitorleonardo8@gmail.com>
2024-03-04 16:33:28 -0300

    Adding exception error

--- converter.py
+++ converter.py
@@ -6,19 +6,31 @@ kelvin = 273.15
 pg = input("Which converter do you want to use?? \n (1) Miles to Km: \n (2) Degrees to Farenheit: \n (3) Degrees to Kelvin: ")
 
 if pg == "1":
-    question_miles = int(input("Quantas milhas: "))
-    total_kms = question_miles * miles_to_km
-    print("{} miles equals {:.1f} km's".format(question_miles, total_kms))
+    try:
+        question_miles = float(input("How many miles? "))
+        total_kms = question_miles * miles_to_km
+        print("{} miles equals {:.1f} kms".format(question_miles, total_kms))
+
+    except ValueError:
+        print("Please, enter a valid number for Miles!")
 
 elif pg == "2":
-    question_degrees = int(input("How  many degress? "))
-    result = (question_degrees * farenheit_factor) + farenheit_offset
-    print("{} degrees equals {:.1f} farenheit".format(question_degrees, result))
+    try:
+        question_degrees = float(input("How  many degress? "))
+        result = (question_degrees * farenheit_factor) + farenheit_offset
+        print("{} degrees equals {:.1f} farenheit".format(question_degrees, result))
+
+    except ValueError:
+        print("Please, enter a valid number for Degrees!")
 
 elif pg == "3":
-    question_kelvin = int(input("How  many degress? "))
-    result = question_kelvin + kelvin
-    print("{} degrees equals {:.2f} kelvin".format(question_kelvin, result))
+    try:
+        question_kelvin = float(input("How  many degress? "))
+        result = question_kelvin + kelvin
+        print("{} degrees equals {:.2f} kelvin".format(question_kelvin, result))
+
+    except ValueError:
+        print("Please, enter a valid number for Degrees!")
 
 else:
-    print("Please, choose option 1 or 2!")
+    print("Please, choose option 1, 2 or 3!")
